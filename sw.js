dbecfeaaca5d039bd5778cb24c7ccee7e2acf08c
trummobanacc <hungdp608@gmail.com>
2026-08-19 19:04:38 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = "hung-admin-v4";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./sw.js",
+    "./1787140316601.png"
+];
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(
+                    cache => cache.addAll(FILES)
+                )
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys().then(
+                keys =>
+
+                    Promise.all(
+
+                        keys
+                            .filter(
+                                key =>
+                                    key !== CACHE_NAME
+                            )
+                            .map(
+                                key =>
+                                    caches.delete(key)
+                            )
+
+                    )
+
+            )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            ).then(
+                cached =>
+                    cached ||
+                    fetch(event.request)
+            )
+
+        );
+
+    }
+);
